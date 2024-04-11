8daaf121c7a294c9e3542552bddda589c1665bb0
Fedoruk Eduard <eduard.fedoruk@nike.com>
2024-04-11 12:49:16 +0300

    remove buffers from line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return {
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diagnostics' },
-                lualine_c = { 'buffers' },
+                lualine_c = { 'filename' },
                 lualine_x = { 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
