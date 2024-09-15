253d6e06db2f66fe3f4e7c644a515924b3ba8128
Fedoruk Eduard <eduard.fedoruk@nike.com>
2024-09-16 00:48:21 +0300

    theme fixes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -69,5 +69,6 @@ return {
 
         -- setup must be called before loading
         vim.cmd.colorscheme "catppuccin"
+        vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#a5adce' })
     end,
 }
