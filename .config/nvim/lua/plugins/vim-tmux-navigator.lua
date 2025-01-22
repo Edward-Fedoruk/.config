25d2fd9964ed44265a22af8cf9fcecdcd916c1c5
Eduard Fedoruk <ed.fedorukk@gmail.com>
2025-01-22 14:02:15 +0200

    config wezterm as tmux

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,17 +1,3 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
-	keys = {
-		{ "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-	},
+    "mrjones2014/smart-splits.nvim",
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,14 +3,6 @@ local wezterm = require 'wezterm';
 
 config.enable_csi_u_key_encoding = true
 
-config.keys = {
-    -- Remap Option + Left Arrow to Alt + B (backward-word)
-    { key = "LeftArrow", mods = "OPT", action = wezterm.action { SendKey = { key = "b", mods = "ALT" } } },
-
-    -- Remap Option + Right Arrow to Alt + F (forward-word)
-    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendKey = { key = "f", mods = "ALT" } } },
-}
-
 config.color_scheme = 'Catppuccin Macchiato'
 config.window_background_opacity = 1
 config.hide_tab_bar_if_only_one_tab = true
@@ -30,4 +22,78 @@ config.window_padding = {
     bottom = 2,
 }
 config.font_size = 14.0
+
+-- tmux like setup
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+local function is_vim(pane)
+    return pane:get_user_vars().IS_NVIM == 'true'
+end
+
+local function is_vim(pane)
+    local process_name = string.gsub(pane:get_foreground_process_name(), '(.*[/\\])(.*)', '%2')
+    return process_name == 'nvim' or process_name == 'vim'
+end
+
+local direction_keys = {
+    h = 'Left',
+    j = 'Down',
+    k = 'Up',
+    l = 'Right',
+}
+
+local function split_nav(resize_or_move, key)
+    return {
+        key = key,
+        mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+        action = wezterm.action_callback(function(win, pane)
+            if is_vim(pane) then
+                win:perform_action({
+                    SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+                }, pane)
+            else
+                if resize_or_move == 'resize' then
+                    win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+                else
+                    win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+                end
+            end
+        end),
+    }
+end
+
+config.keys = {
+    -- splitting
+    {
+        mods   = "LEADER",
+        key    = "-",
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+    },
+    {
+        mods   = "LEADER",
+        key    = "|",
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+    },
+    {
+        mods = 'LEADER',
+        key = 'm',
+        action = wezterm.action.TogglePaneZoomState
+    },
+
+    -- Remap Option + Left Arrow to Alt + B (backward-word)
+    { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendKey = { key = "b", mods = "ALT" } } },
+
+    -- Remap Option + Right Arrow to Alt + F (forward-word)
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendKey = { key = "f", mods = "ALT" } } },
+
+    split_nav('move', 'h'),
+    split_nav('move', 'j'),
+    split_nav('move', 'k'),
+    split_nav('move', 'l'),
+    split_nav('resize', 'h'),
+    split_nav('resize', 'j'),
+    split_nav('resize', 'k'),
+    split_nav('resize', 'l'),
+}
+
 return config
