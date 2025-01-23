65b3bfaead5d7f8f00bbd46c05511d2c9e6eef65
Eduard Fedoruk <ed.fedorukk@gmail.com>
2025-01-23 13:11:44 +0200

    configure workspaces in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ config.enable_csi_u_key_encoding = true
 
 config.color_scheme = 'Catppuccin Macchiato'
 config.window_background_opacity = 1
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.window_frame = {
     active_titlebar_bg = '#1e1e2f'
 }
@@ -24,6 +24,46 @@ config.window_padding = {
     bottom = 2,
 }
 config.font_size = 14.0
+
+wezterm.on('update-right-status', function(window, pane)
+    window:set_right_status(window:active_workspace())
+end)
+
+filter = function(tbl, callback)
+    local filt_table = {}
+
+    for i, v in ipairs(tbl) do
+        if callback(v, i) then
+            table.insert(filt_table, v)
+        end
+    end
+    return filt_table
+end
+
+kill_workspace = function(workspace)
+    local success, stdout =
+        wezterm.run_child_process({ "/opt/homebrew/bin/wezterm", "cli", "list", "--format=json" })
+
+    if success then
+        local json = wezterm.json_parse(stdout)
+        if not json then
+            return
+        end
+
+        local workspace_panes = filter(json, function(p)
+            return p.workspace == workspace
+        end)
+
+        for _, p in ipairs(workspace_panes) do
+            wezterm.run_child_process({
+                "/opt/homebrew/bin/wezterm",
+                "cli",
+                "kill-pane",
+                "--pane-id=" .. p.pane_id,
+            })
+        end
+    end
+end
 
 config.keys = {
     {
@@ -45,6 +85,69 @@ config.keys = {
         mods = 'LEADER',
         key = 'm',
         action = wezterm.action.TogglePaneZoomState
+    },
+    {
+        key = 'f',
+        mods = 'LEADER',
+        action = wezterm.action.ShowLauncherArgs {
+            flags = 'FUZZY|WORKSPACES',
+        },
+    },
+    {
+        key = "k",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(window)
+            local w = window:active_workspace()
+            kill_workspace(w)
+        end),
+    },
+    {
+        key = 'r',
+        mods = 'LEADER',
+        action = wezterm.action.PromptInputLine {
+            description = wezterm.format {
+                { Attribute = { Intensity = 'Bold' } },
+                { Foreground = { AnsiColor = 'Fuchsia' } },
+                { Text = 'Enter new name for workspace' },
+            },
+            action = wezterm.action_callback(function(window, pane, line)
+                -- line will be `nil` if they hit escape without entering anything
+                -- An empty string if they just hit enter
+                -- Or the actual line of text they wrote
+                if line then
+                    window:perform_action(
+                        wezterm.mux.rename_workspace(
+                            wezterm.mux.get_active_workspace(),
+                            line
+                        )
+                    )
+                end
+            end),
+        },
+    },
+    {
+        key = 'w',
+        mods = 'LEADER',
+        action = wezterm.action.PromptInputLine {
+            description = wezterm.format {
+                { Attribute = { Intensity = 'Bold' } },
+                { Foreground = { AnsiColor = 'Fuchsia' } },
+                { Text = 'Enter name for new workspace' },
+            },
+            action = wezterm.action_callback(function(window, pane, line)
+                -- line will be `nil` if they hit escape without entering anything
+                -- An empty string if they just hit enter
+                -- Or the actual line of text they wrote
+                if line then
+                    window:perform_action(
+                        wezterm.action.SwitchToWorkspace {
+                            name = line,
+                        },
+                        pane
+                    )
+                end
+            end),
+        },
     },
 
     -- Remap Option + Left Arrow to Alt + B (backward-word)
